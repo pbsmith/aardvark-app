c79118bf64d6c01fb9ddcd49715faa338d8e15d8
Pendle Smith <pbsmith2008@yahoo.com>
2023-12-06 09:43:06 -0500

    Card Server Portion

---
+++ dotnet/Capstone/DAO/CardSqlDao.cs
@@ -0,0 +1,50 @@
+﻿using Capstone.DAO.Interface;
+using Capstone.Exceptions;
+using Capstone.Models;
+using System.Data.SqlClient;
+
+namespace Capstone.DAO
+
+{
+    public class CardSqlDao : ICardDao
+    {
+        private readonly string connectionString;
+
+        public CardSqlDao(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public Card createCard(Card card)
+        {
+
+            string sql = "INSERT INTO cards (term, definition, deck_id, user_id) " +
+                "OUTPUT INSERTED.card_id " +
+                "VALUES (@term, @definition, @deck_id, @user_id)";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@term", card.term);
+                        cmd.Parameters.AddWithValue("@definition", card.definition);
+                        cmd.Parameters.AddWithValue("@deck_id", card.deckId);
+                        cmd.Parameters.AddWithValue("@user_id", card.userId);
+
+                        card.cardId = (int)cmd.ExecuteScalar();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new DaoException("SQL exception occurred", ex);
+            }
+
+            return card;
+
+        }
+
+    }
+}
---
+++ dotnet/Capstone/DAO/Interface/ICardDao.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Capstone.Models;
+
+
+namespace Capstone.DAO.Interface
+{
+    public interface ICardDao
+    {
+        Card createCard(Card card);
+    }
+}
