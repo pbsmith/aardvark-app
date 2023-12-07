a070e1feb61217082b4f558360c892409093f0c8
Pendle Smith <pbsmith2008@yahoo.com>
2023-12-07 09:15:53 -0500

    Edit Card Added

--- vue/src/components/CardDetails.vue
+++ vue/src/components/CardDetails.vue
@@ -4,12 +4,32 @@
             <p>{{ currentCard.term }}</p>
             <p>{{ currentCard.definition }}</p>
         </div>
+        <button v-on:click="editCard" v-if="!isFormDisplayed">Edit</button>
+        <button v-on:click="cancelCard" v-if="isFormDisplayed">Cancel</button>
     </div>
+    <CardForm v-if="isFormDisplayed" v-bind:card="currentCard"/>
 </template>
 
 <script>
+import CardForm from '../components/CardForm.vue';
 
 export default {
+    data() {
+        return {
+            isFormDisplayed: false,
+        }
+    },
+    methods: {
+        editCard() {
+                this.isFormDisplayed = true;
+        },
+        cancelCard() {
+            this.isFormDisplayed = false;
+        }
+    },
+    components: {
+    CardForm
+},
     props:
         ['currentCard']
 }
