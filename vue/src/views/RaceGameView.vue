cf32e6b5918d2b59942e45d876d8f3b577625408
Sara Nippert <16saranippert@gmail.com>
2023-12-11 13:47:55 -0500

    Race Game Displays shuffled cards

--- vue/src/views/RaceGameView.vue
+++ vue/src/views/RaceGameView.vue
@@ -1,17 +1,19 @@
 <template>
     <h1>Card Race</h1>
     <!--Display an error instead of game if not enough cards-->
-    <div v-if="!canPlay">
+    <div v-if="!checkPlayability">
         <h2>Your deck must contain at least 10 cards to play this game. Please add more cards and try again.</h2>
     </div>
     <!--Display game if enough cards-->
-    <div v-if="canPlay">
+    <div v-if="checkPlayability">
         <div class="termCard">
+            <h2>Current Card</h2>
             <div class="cardcontainer">
                 {{ correctCard.term }}
             </div>
         </div>
         <div class="possibleDefinitions">
+            <h2>Definitions</h2>
             <div class="cardcontainer" v-for="card in roundCards" v-bind:key="card.cardId">
                 {{ card.definition }}
             </div>
@@ -37,11 +39,22 @@ export default {
             cards: [],
             correctCard: {},
             roundCards: [],
-            canPlay: false,
             isOutOfCards: false,
         }
     },
     methods: {
+        /** checks to see if user has 10 or more cards.
+         * if user has 10+ cards, game displays.
+         * if user has less than 10 cards, error displays.
+         */
+        checkPlayability() {
+            if (this.cards.length >= 10) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
         /** if enough cards in set, 
          *  Gets a deck of cards and allows user to play
          *  if not enough cards, doesn't allow user to play.
@@ -49,32 +62,31 @@ export default {
         getShuffledCards() {
             CardService.getCardsByDeckId(this.$route.params.deckId)
                 .then(response => {
+                    console.log(this.cards)
                     this.cards = response.data;
-                    if (this.cards.length >= 10) {
-                        this.shuffledCards = _.shuffle(this.cards);
-                        this.canPlay = true;
-                        this.getRoundCards();
-                    }
-                    else {
-                        this.canPlay = false;
-                    }
+                    this.shuffledCards = _.shuffle(this.cards);
+                    this.getRoundCards();
                 })
                 .catch(error => {
                     this.handleError();
                 })
         },
-        getRoundCards() {
-            /** add 4 cards to the round cards
+        /** add 4 cards to the round cards
              *  remove the card being tested from the card list */
+        getRoundCards() {
             let counter = 0;
             if (this.cards.length >= 4) {
                 this.correctCard = this.cards.pop();
                 this.roundCards.push(this.correctCard);
-                while (counter > 3) {
-                    let indexNum = Math.random(this.cards.count - 1, 0);
-                    this.roundCards.push(this.cards[indexNum]);
+                while (counter < 3) {
+                    this.cards = _.shuffle(this.cards);
+
+                    let nextCard = this.cards.pop();
+                    this.roundCards.push(nextCard);
+
                     counter++;
                 }
+                console.log('in get roundCards', this.roundCards)
                 this.roundCards = _.shuffle(this.roundCards);
             }
             else {
@@ -84,8 +96,10 @@ export default {
         }
     },
     created() {
-        this.getShuffledCards()
-    }
+        this.checkPlayability();
+        this.getShuffledCards();
+        this.getRoundCards();
 
+    }
 }
 </script>
