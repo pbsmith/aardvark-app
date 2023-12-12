223aed56d140affdc09f17bbc31938b8e6c1b70a
Pendle Smith <pbsmith2008@yahoo.com>
2023-12-12 10:54:31 -0500

    started matching game

--- vue/src/views/MemoryCardView.vue
+++ vue/src/views/MemoryCardView.vue
@@ -1,15 +1,39 @@
 <template>
     <div>
-        <MemoryCardGame />
+        <button v-on:click="startGame" v-if="!isStarted">Start</button>
+    </div>
+    <div>
+        <MemoryCardGame v-if="isStarted" v-bind:cards="cards"/>
     </div>
 </template>
 
 <script>
 import MemoryCardGame from '../components/MemoryCardGame.vue'
+import CardService from '../services/CardService'
 
 export default {
+    data() {
+        return {
+            isStarted: false,
+            cards: []
+        }
+    },
     components: {
         MemoryCardGame
+    },
+    created() {
+        CardService
+            .getCardsByDeckId(this.$route.params.deckId)
+            .then(response => {
+                if(response.status === 200) {
+                    this.cards = response.data;
+                }
+            })
+    },
+    methods: {
+        startGame() {
+            this.isStarted = true;
+        }    
     }
 }
 </script>
